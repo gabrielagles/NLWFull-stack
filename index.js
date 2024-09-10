5c4476c5d99fb89745b8ec5c1b60b7bf28ba8765
Gabriel Agles <gabriel.aglesss@gmail.com>
2024-09-10 14:43:41 -0300

    fundamentos 002

---
+++ index.js
@@ -0,0 +1,16 @@
+// array, objetos
+let meta = {
+    value: 'ler um livro por mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
+
